d8a2cd6f063e7888172640842d336b371e4fcd28
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-11-26 10:55:15 +0100

    fix mobile-menu-js

--- src/js/mobile-menu/mobile-menu.js
+++ src/js/mobile-menu/mobile-menu.js
@@ -1,5 +1,3 @@
-import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
-
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
 const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -12,12 +10,6 @@ const toggleMenu = () => {
     openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
   openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
   mobileMenu.classList.toggle('is-open');
-
-  // if (!isMenuOpen) {
-  //   disableBodyScroll(mobileMenu);
-  // } else {
-  //   enableBodyScroll(mobileMenu);
-  // }
 
   if (!isMenuOpen) {
     document.body.style.overflow = 'hidden';
@@ -38,5 +30,5 @@ window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
   if (!e.matches) return;
   mobileMenu.classList.remove('is-open');
   openMenuBtn.setAttribute('aria-expanded', false);
-  enableBodyScroll(document.body);
+  document.body.style.overflow = 'auto';
 });
