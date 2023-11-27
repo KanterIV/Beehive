ffed6c3f84426e27095546ea3ee35e96c86d275b
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-11-27 20:22:50 +0100

    fix scroll-up btn

--- src/js/scroll-up/scrollUp.js
+++ src/js/scroll-up/scrollUp.js
@@ -1,13 +1,10 @@
-//get button element
 const elementsSet = {
   case: document.querySelector('.buttonup-case'),
   upButton: document.querySelector('.buttonup'),
 };
 
-// event handler
-let upButVieW = evt => {
+const upButVieW = evt => {
   switch (evt.type) {
-    // visibility/hidden scrolup button
     case 'scroll':
       if (Math.floor(window.scrollY) >= window.innerHeight / 2) {
         elementsSet.case.style.visibility = 'visible';
@@ -17,22 +14,19 @@ let upButVieW = evt => {
         elementsSet.upButton.classList.remove('anima-up');
       }
       break;
-    // visibility/hidden border
-    case 'mouseover':
-      elementsSet.case.style.outline = '2px solid grey';
 
+    case 'mouseover':
+      elementsSet.case.style.border = '2px solid var(--color-gray-60)';
       break;
     case 'mouseout':
-      elementsSet.case.style.outline = 'none';
-
+      elementsSet.case.style.border = 'none';
       break;
-    // scroll up
     case 'click':
       window.scrollTo(0, 0);
       break;
     default:
       elementsSet.upButton.style.visibility = 'hidden';
-      elementsSet.upButton.style.outline = 'none';
+      elementsSet.case.style.border = 'none';
       break;
   }
 };
@@ -41,27 +35,3 @@ document.addEventListener('scroll', upButVieW);
 elementsSet.case.addEventListener('mouseover', upButVieW);
 elementsSet.case.addEventListener('mouseout', upButVieW);
 elementsSet.upButton.addEventListener('click', upButVieW);
-
-window.addEventListener('resize', checkScroll);
-checkScroll();
-function checkScroll() {
-  if (document.body.offsetWidth >= 1440) {
-    elementsSet.case.style.left = `${document.body.offsetWidth / 2 + 620}px`;
-    elementsSet.case.style.top = `80vh`;
-  } else if (document.body.offsetWidth >= 768) {
-    // elementsSet.case.style.left = `${document.body.offsetWidth - 80}px`;
-    elementsSet.case.style.left = `${document.body.offsetWidth / 2 + 290}px`;
-    elementsSet.case.style.top = `80vh`;
-  } else if (
-    document.body.offsetWidth > 375 &&
-    document.body.offsetWidth < 768
-  ) {
-    elementsSet.case.style.left = `${
-      document.body.offsetWidth / 2 + document.body.offsetWidth / 3
-    }px`;
-    elementsSet.case.style.top = `75vh`;
-  } else if (document.body.offsetWidth <= 375) {
-    elementsSet.case.style.left = `${document.body.offsetWidth / 2 + 80}px`;
-    elementsSet.case.style.top = `70vh`;
-  }
-}
