8842bc376c89b54df157dd568871906238c2a72b
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-11-29 19:58:15 +0100

    fix btn scroll

--- src/js/scroll-up/scrollUp.js
+++ src/js/scroll-up/scrollUp.js
@@ -8,11 +8,17 @@ const upButVieW = evt => {
     case 'scroll':
       if (Math.floor(window.scrollY) >= window.innerHeight / 2) {
         elementsSet.case.style.visibility = 'visible';
-        // elementsSet.upButton.classList.add('anima-up');
+        elementsSet.upButton.classList.add('anima-up');
       } else {
         elementsSet.case.style.visibility = 'hidden';
-        // elementsSet.upButton.classList.remove('anima-up');
+        elementsSet.upButton.classList.remove('anima-up');
       }
+      break;
+    case 'mouseover':
+      elementsSet.upButton.classList.add('buttonup-hover');
+      break;
+    case 'mouseout':
+      elementsSet.upButton.classList.remove('buttonup-hover');
       break;
     case 'click':
       window.scrollTo(0, 0);
@@ -24,4 +30,6 @@ const upButVieW = evt => {
 };
 
 document.addEventListener('scroll', upButVieW);
+elementsSet.case.addEventListener('mouseover', upButVieW);
+elementsSet.case.addEventListener('mouseout', upButVieW);
 elementsSet.upButton.addEventListener('click', upButVieW);
