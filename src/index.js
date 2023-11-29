67389c3739538da1ee565809b283a5ce1d0b1fbb
KanterIV <126972573+KanterIV@users.noreply.github.com>
2023-11-29 18:22:57 +0100

    added definiteness logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/modal/modal';
 import './js/mobile-menu/mobile-menu';
 import './js/scroll-up/scrollUp';
+import './js/definiteness/definiteness';
---
+++ src/js/definiteness/definiteness.js
@@ -0,0 +1,74 @@
+const defListItemsArr = [...document.querySelectorAll('.definiteness-item')];
+const amount = document.querySelector('.amount');
+
+defListItemsArr.map(item => item.addEventListener('click', onGigabyteСlick));
+
+let gigabyteId = 3;
+
+gygabyteColorChange(gigabyteId);
+
+function onGigabyteСlick(event) {
+  gigabyteId = Number(event.currentTarget.id);
+  console.log(gigabyteId);
+  gygabyteColorChange(gigabyteId);
+  amountChange(gigabyteId);
+}
+
+function gygabyteColorChange(lastElementNum) {
+  colorChecker(defListItemsArr);
+  for (let i = 0; i <= lastElementNum; i += 1) {
+    const arr1 = [...defListItemsArr[i].children];
+    arr1.map(item => {
+      if (item.className === 'definiteness-chexbox') {
+        item.classList.add('def-chexbox-bgcolor');
+        return;
+      }
+      item.classList.add('def-chexbox-btn-color');
+    });
+  }
+}
+
+function colorChecker(arr) {
+  arr.map(arrItem => {
+    const itemChildren = [...arrItem.children];
+    itemChildren.map(item => {
+      item.classList.remove('def-chexbox-bgcolor');
+      item.classList.remove('def-chexbox-btn-color');
+    });
+  });
+}
+
+function amountChange(id) {
+  switch (id) {
+    case 0:
+      amount.textContent = '150';
+      break;
+    case 1:
+      amount.textContent = '300';
+      break;
+    case 2:
+      amount.textContent = '525';
+      break;
+    case 3:
+      amount.textContent = '925';
+      break;
+    case 4:
+      amount.textContent = '1050';
+      break;
+    case 5:
+      amount.textContent = '1425';
+      break;
+    case 6:
+      amount.textContent = '1850';
+      break;
+    case 7:
+      amount.textContent = '2275';
+      break;
+    case 8:
+      amount.textContent = '3050';
+      break;
+    case 9:
+      amount.textContent = '4000';
+      break;
+  }
+}
